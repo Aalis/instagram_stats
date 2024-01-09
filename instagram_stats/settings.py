af39ebd6bf4f0d5432251f041133ae8c9d1e5b1a
aalis <andreylisogor@gmail.com>
2024-01-09 20:14:26 +0200

    celery added

--- instagram_stats/settings.py
+++ instagram_stats/settings.py
@@ -145,3 +145,7 @@ LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
 
 SIGNUP_REDIRECT_URL = "home"
+
+# Celery configuration
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
