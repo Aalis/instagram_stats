619d24ffe3dca00fe1f666ab2f48cdc558c8a796
aalis <andreylisogor@gmail.com>
2024-01-09 18:26:50 +0200

    added inst_profile functionality

--- inst_profiles/views.py
+++ inst_profiles/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import InstProfile
+from .forms import InstProfileForm
 
-# Create your views here.
+
+def add_instprofile(request):
+    if request.method == "POST":
+        form = InstProfileForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("instprofile_list")
+    else:
+        form = InstProfileForm()
+    return render(request, "add_instprofile.html", {"form": form})
+
+
+def instprofile_list(request):
+    inst_profiles = InstProfile.objects.all()
+    return render(request, "instprofile_list.html", {"inst_profiles": inst_profiles})
