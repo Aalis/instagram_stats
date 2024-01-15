55a04a4d4141a0742b85dcbb445b56fb8d1778f1
aalis <andreylisogor@gmail.com>
2024-01-15 03:43:58 +0200

    parse instagram profile and add followers count to database

--- inst_profiles/models.py
+++ inst_profiles/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class InstProfile(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    link = models.URLField()
-    followers_count = models.PositiveIntegerField()
+    name = models.CharField(max_length=255, unique=True)
+    link = models.URLField(unique=True)
+    followers_count = models.PositiveIntegerField(blank=True, null=True)
 
     def __str__(self):
         return self.name
